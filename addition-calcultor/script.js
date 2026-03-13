b2d74e42d6dfbca7e518cf0b7ffc61427c57048f
rohitseera9-hue <rohitseera9@gmail.com>
2026-03-13 15:35:39 +0530

    Delete addition-calcultor directory

--- addition-calcultor/script.js
+++
@@ -1,9 +0,0 @@
-function addNumbers()
-{
-    let n1 = Number(document.getElementById("num1").value);
-    let n2 = Number(document.getElementById("num2").value);
-
-    let sum = n1 + n2;
-
-    document.getElementById("result").innerText = "Result: " + sum;
-}
